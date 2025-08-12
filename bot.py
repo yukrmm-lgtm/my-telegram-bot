9fa5620531481f18cf9382f4b95a13ea1f602ad8
yukrmm-lgtm <yukrmm@gmail.com>
2025-08-12 21:26:16 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,6 @@ from telegram.ext import filters
 
 TOKEN = "8235892772:AAEUSJHCF_eUBkJtS3gUdRHbRQIf6W7HfiU" 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Привет! Я твой бот!')
-
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Вы сказали: {update.message.text}")
     
@@ -24,7 +21,6 @@ def main():
     application = Application.builder().token(TOKEN).build()
     
     # Добавляем обработчики
-    application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.add_handler(CommandHandler("list_commands", list_commands))
     

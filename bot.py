20db528abf2443890f7636e1b92c4cc5f68de13a
yukrmm-lgtm <yukrmm@gmail.com>
2025-08-12 22:00:51 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 from telegram.ext import filters
 
 TOKEN = "8235892772:AAEUSJHCF_eUBkJtS3gUdRHbRQIf6W7HfiU" 
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Вы сказали: {update.message.text}")
     
 # Добавьте эту функцию
 async def list_commands(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -21,7 +18,6 @@ def main():
     application = Application.builder().token(TOKEN).build()
     
     # Добавляем обработчики
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.add_handler(CommandHandler("list_commands", list_commands))
     
     # Запускаем бота
